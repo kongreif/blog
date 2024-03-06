2df40d77a35ab0d28b7b602d7d3188570d9d36a4
kongreif <konstantin.greif@mailfence.com>
2024-03-06 17:52:05 +0100

    Create first version of post controller with spec

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,45 @@
-class PostsController < ApplicationController
-  def new
-  end
+# frozen_string_literal: true
 
-  def create
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
-  def index
+  def new
+    @post = Post.new
   end
 
   def edit
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
 
   def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content, :published)
   end
 end
--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,46 +1,55 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Posts", type: :request do
-  describe "GET /new" do
-    it "returns http success" do
-      get "/posts/new"
+RSpec.describe 'Posts' do
+  let(:post1) { create(:post) }
+  let(:valid_post_params) do
+    { post: { title: Faker::Lorem.sentence, content: Faker::Lorem.paragraph } }
+  end
+
+  describe 'GET /new' do
+    it 'returns http success' do
+      get new_post_path
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /create" do
-    it "returns http success" do
-      get "/posts/create"
+  describe 'POST /create' do
+    it 'creates a new post and redirects to the posts show page' do
+      expect do
+        post posts_path, params: valid_post_params
+      end.to change(Post, :count).by(1)
+
+      expect(response).to redirect_to(post_path(Post.last))
+    end
+  end
+
+  describe 'GET /show' do
+    it 'returns http success' do
+      get post_path(post1)
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /show" do
-    it "returns http success" do
-      get "/posts/show"
+  describe 'GET /index' do
+    it 'returns http success' do
+      get posts_path
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /index" do
-    it "returns http success" do
-      get "/posts/index"
+  describe 'GET /edit' do
+    it 'returns http success' do
+      get edit_post_path(post1)
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /edit" do
-    it "returns http success" do
-      get "/posts/edit"
-      expect(response).to have_http_status(:success)
+  describe 'GET /update' do
+    it 'returns http success' do
+      patch post_path(post1), params: valid_post_params
+      expect(response).to redirect_to(post_path(post1))
     end
   end
-
-  describe "GET /update" do
-    it "returns http success" do
-      get "/posts/update"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
