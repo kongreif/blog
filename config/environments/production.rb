91749ffca436744b11b9c8a3601290f9d0aa4329
kongreif <konstantin.greif@mailfence.com>
2024-03-06 14:46:19 +0100

    Add devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,9 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "blog_production"
 
+  # TODO: Set up a real email delivery method for production.
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
