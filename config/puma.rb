7905af75d679dc000e7bc651703b230a1b153c9e
Konstantin Greif <konstantin.greif@mailfence.com>
2024-08-01 23:00:06 +0200

    Remove logging from puma

--- config/puma.rb
+++ config/puma.rb
@@ -35,5 +35,3 @@ pidfile ENV.fetch('PIDFILE', 'tmp/pids/server.pid')
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
-
-stdout_redirect '/home/kongreif/logs/puma/out.log', '/home/isabell/logs/puma/err.log', true
