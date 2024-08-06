51b02712fec9f4688e86efb4a8b00d28e2c64a7c
Konstantin Greif <konstantin.greif@mailfence.com>
2024-08-06 23:10:16 +0200

    Add kongreif.com host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,4 +100,6 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
   config.hosts << 'kongreif.onrender.com'
+  config.hosts << 'kongreif.com'
+  config.hosts << 'www.kongreif.com'
 end
