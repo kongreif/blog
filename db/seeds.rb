a3806c7f706bfbaae0f6fe39369423a5ab9f197d
Konstantin Greif <konstantin.greif@mailfence.com>
2024-10-03 18:22:04 +0200

    Add seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,29 @@
 # frozen_string_literal: true
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+
+puts 'Start seeding'
+puts '-----------------------------'
+puts 'Remove old data'
+
+Post.destroy_all
+
+puts 'Adding posts'
+
+5.times do
+  content = <<~TEXT
+    #{Faker::Lorem.paragraphs(number: (3..9).to_a.sample).join(' ')}\n
+    ```ruby
+    def test_function(num1, num2)
+      num1 + num2
+    end
+    ```
+    This is some `inline code` to test.
+    ## #{Faker::Lorem.words(number: (1..9).to_a.sample).join(' ')}
+    #{Faker::Lorem.paragraphs(number: (3..9).to_a.sample).join(' ')}\n
+  TEXT
+
+  post = Post.create(title: Faker::Lorem.words(number: (1..9).to_a.sample).join(' '), content:, excerpt: Faker::Lorem.paragraph)
+  puts "Add '#{post.title}' post"
+end
+
+puts '-----------------------------'
+puts 'Finished seeding'
